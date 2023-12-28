1b7675253d0198a5c7d526ec8b141b561c11ef57
MrGeek-zrh <mrgeek-zrh@outlook.com>
2023-12-28 14:31:21 +0800

    feat(DP): rewrite arf_tb in verilator and format verilog code by verible

--- rtl/core/DP/SrcOprManager.v
+++ rtl/core/DP/SrcOprManager.v
@@ -1,20 +1,17 @@
 `include "consts/Consts.vh"
 
-module SrcOprManager(
-  input wire arf_busy_i,
-  input	wire [`DATA_LEN-1:0] arf_data_i,
-  input wire [`RRF_SEL-1:0] arf_rrftag_i,
-  input wire rrf_valid_i,
-  input wire [`DATA_LEN-1:0] rrf_data_i,
-  input wire src_eq_zero_i,
-  output wire [`DATA_LEN-1:0] src_o,
-  output wire 		       ready_o
+module SrcOprManager (
+    input  wire                 arf_busy_i,
+    input  wire [`DATA_LEN-1:0] arf_data_i,
+    input  wire [ `RRF_SEL-1:0] arf_rrftag_i,
+    input  wire                 rrf_valid_i,
+    input  wire [`DATA_LEN-1:0] rrf_data_i,
+    input  wire                 src_eq_zero_i,
+    output wire [`DATA_LEN-1:0] src_o,
+    output wire                 ready_o
 );
 
-   assign src_o = src_eq_zero_i ? `DATA_LEN'b0 :
-		~arf_busy_i ? arf_data_i:
-		rrf_valid_i ? rrf_data_i:
-		{26'b0,arf_rrftag_i};
-	  // 表明src是否已经就绪，即src中已经是data了，而不是rrftag
-   assign ready_o = src_eq_zero_i | ~arf_busy_i | rrf_valid_i;
+    assign src_o   = src_eq_zero_i ? `DATA_LEN'b0 : ~arf_busy_i ? arf_data_i : rrf_valid_i ? rrf_data_i : {26'b0, arf_rrftag_i};
+    // 表明src是否已经就绪，即src中已经是data了，而不是rrftag
+    assign ready_o = src_eq_zero_i | ~arf_busy_i | rrf_valid_i;
 endmodule
--- rtl/core/DP/SynRam.v
+++ rtl/core/DP/SynRam.v
@@ -2,92 +2,84 @@
 `default_nettype none
 
 module SynRam_2r2w #(
-		       parameter BRAM_ADDR_WIDTH = `ADDR_LEN,
-		       parameter BRAM_DATA_WIDTH = `DATA_LEN,
-		       parameter DATA_DEPTH      = 32
-		       ) 
-   (
-    input wire 			     clk_i,
-    input wire [BRAM_ADDR_WIDTH-1:0] raddr1,
-    input wire [BRAM_ADDR_WIDTH-1:0] raddr2,
-    output reg [BRAM_DATA_WIDTH-1:0] rdata1,
-    output reg [BRAM_DATA_WIDTH-1:0] rdata2,
-    input wire [BRAM_ADDR_WIDTH-1:0] waddr1,
-    input wire [BRAM_ADDR_WIDTH-1:0] waddr2,
-    input wire [BRAM_DATA_WIDTH-1:0] wdata1,
-    input wire [BRAM_DATA_WIDTH-1:0] wdata2,
-    input wire 			     we1,
-    input wire 			     we2
-    );
-   
-   reg [BRAM_DATA_WIDTH-1:0] 			      mem [0:DATA_DEPTH-1];
+    parameter BRAM_ADDR_WIDTH = `ADDR_LEN,
+    parameter BRAM_DATA_WIDTH = `DATA_LEN,
+    parameter DATA_DEPTH      = 32
+) (
+    input  wire                       clk_i,
+    input  wire [BRAM_ADDR_WIDTH-1:0] raddr1,
+    input  wire [BRAM_ADDR_WIDTH-1:0] raddr2,
+    output reg  [BRAM_DATA_WIDTH-1:0] rdata1,
+    output reg  [BRAM_DATA_WIDTH-1:0] rdata2,
+    input  wire [BRAM_ADDR_WIDTH-1:0] waddr1,
+    input  wire [BRAM_ADDR_WIDTH-1:0] waddr2,
+    input  wire [BRAM_DATA_WIDTH-1:0] wdata1,
+    input  wire [BRAM_DATA_WIDTH-1:0] wdata2,
+    input  wire                       we1,
+    input  wire                       we2
+);
 
-   always @ (posedge clk_i) begin
-      rdata1 <= mem[raddr1];
-      rdata2 <= mem[raddr2];
-      if (we1)
-	mem[waddr1] <= wdata1;
-      if (we2)
-	mem[waddr2] <= wdata2;
-   end
-endmodule // SynRam_2r2w
+  reg [BRAM_DATA_WIDTH-1:0] mem[0:DATA_DEPTH-1];
+
+  always @(posedge clk_i) begin
+    rdata1 <= mem[raddr1];
+    rdata2 <= mem[raddr2];
+    if (we1) mem[waddr1] <= wdata1;
+    if (we2) mem[waddr2] <= wdata2;
+  end
+endmodule  // SynRam_2r2w
 
 
 module SynRam #(
-		       parameter BRAM_ADDR_WIDTH = `ADDR_LEN,
-		       parameter BRAM_DATA_WIDTH = `DATA_LEN,
-		       parameter DATA_DEPTH      = 32
-		       ) 
-   (
-    input wire 			      clk_i,
-    input wire [BRAM_ADDR_WIDTH-1:0]  raddr1,
-    input wire [BRAM_ADDR_WIDTH-1:0]  raddr2,
-    input wire [BRAM_ADDR_WIDTH-1:0]  raddr3,
-    input wire [BRAM_ADDR_WIDTH-1:0]  raddr4,
+    parameter BRAM_ADDR_WIDTH = `ADDR_LEN,
+    parameter BRAM_DATA_WIDTH = `DATA_LEN,
+    parameter DATA_DEPTH      = 32
+) (
+    input  wire                       clk_i,
+    input  wire [BRAM_ADDR_WIDTH-1:0] raddr1,
+    input  wire [BRAM_ADDR_WIDTH-1:0] raddr2,
+    input  wire [BRAM_ADDR_WIDTH-1:0] raddr3,
+    input  wire [BRAM_ADDR_WIDTH-1:0] raddr4,
     output wire [BRAM_DATA_WIDTH-1:0] rdata1,
     output wire [BRAM_DATA_WIDTH-1:0] rdata2,
     output wire [BRAM_DATA_WIDTH-1:0] rdata3,
     output wire [BRAM_DATA_WIDTH-1:0] rdata4,
-    input wire [BRAM_ADDR_WIDTH-1:0]  waddr1,
-    input wire [BRAM_ADDR_WIDTH-1:0]  waddr2,
-    input wire [BRAM_DATA_WIDTH-1:0]  wdata1,
-    input wire [BRAM_DATA_WIDTH-1:0]  wdata2,
-    input wire 			      we1,
-    input wire 			      we2
-    );
+    input  wire [BRAM_ADDR_WIDTH-1:0] waddr1,
+    input  wire [BRAM_ADDR_WIDTH-1:0] waddr2,
+    input  wire [BRAM_DATA_WIDTH-1:0] wdata1,
+    input  wire [BRAM_DATA_WIDTH-1:0] wdata2,
+    input  wire                       we1,
+    input  wire                       we2
+);
 
-   SynRam_2r2w 
-     #(BRAM_ADDR_WIDTH, BRAM_DATA_WIDTH, DATA_DEPTH) 
-   mem0(
-	.clk_i(clk_i),
-	.raddr1(raddr1),
-	.raddr2(raddr2),
-	.rdata1(rdata1),
-	.rdata2(rdata2),
-	.waddr1(waddr1),
-	.waddr2(waddr2),
-	.wdata1(wdata1),
-	.wdata2(wdata2),
-	.we1(we1),
-	.we2(we2)
-	);
+  SynRam_2r2w #(BRAM_ADDR_WIDTH, BRAM_DATA_WIDTH, DATA_DEPTH) mem0 (
+      .clk_i(clk_i),
+      .raddr1(raddr1),
+      .raddr2(raddr2),
+      .rdata1(rdata1),
+      .rdata2(rdata2),
+      .waddr1(waddr1),
+      .waddr2(waddr2),
+      .wdata1(wdata1),
+      .wdata2(wdata2),
+      .we1(we1),
+      .we2(we2)
+  );
 
-   SynRam_2r2w 
-     #(BRAM_ADDR_WIDTH, BRAM_DATA_WIDTH, DATA_DEPTH) 
-   mem1(
-	.clk_i(clk_i),
-	.raddr1(raddr3),
-	.raddr2(raddr4),
-	.rdata1(rdata3),
-	.rdata2(rdata4),
-	.waddr1(waddr1),
-	.waddr2(waddr2),
-	.wdata1(wdata1),
-	.wdata2(wdata2),
-	.we1(we1),
-	.we2(we2)
-	);
-   
-endmodule // SynRam_4r2w
+  SynRam_2r2w #(BRAM_ADDR_WIDTH, BRAM_DATA_WIDTH, DATA_DEPTH) mem1 (
+      .clk_i(clk_i),
+      .raddr1(raddr3),
+      .raddr2(raddr4),
+      .rdata1(rdata3),
+      .rdata2(rdata4),
+      .waddr1(waddr1),
+      .waddr2(waddr2),
+      .wdata1(wdata1),
+      .wdata2(wdata2),
+      .we1(we1),
+      .we2(we2)
+  );
+
+endmodule  // SynRam_4r2w
 
 `default_nettype wire
