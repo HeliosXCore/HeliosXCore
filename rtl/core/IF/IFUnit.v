670cb895048861ec4a1ae98dc1ab3aedf724c860
Schedinger <58102563+Schedinger@users.noreply.github.com>
2024-01-03 10:00:04 +0800

    Update IFUnit.v

--- rtl/core/IF/IFUnit.v
+++ rtl/core/IF/IFUnit.v
@@ -4,18 +4,21 @@
 module IFUnit (
     input wire 			  clk_i,
     input wire 			  reset_i,
-    input wire [`ADDR_LEN-1:0] pc_i,
     input wire [4*`INSN_LEN-1:0] idata_i,
 
-    output wire [`ADDR_LEN-1:0] npc_o,
-    output wire [`INSN_LEN-1:0] inst1_o
+    output reg [`ADDR_LEN-1:0] pc_o
+    //output wire [`INSN_LEN-1:0] inst1_o
     //output wire [`INSN_LEN-1:0] inst2_o,
     //output wire 		invalid2_pipe_o
 
 );
 
+    wire [`ADDR_LEN-1:0] npc;
+    wire [`INSN_LEN-1:0] inst1;
+    //wire [`INSN_LEN-1:0] inst2;
+    //wire 		invalid2_pipe;
+
     reg [`ADDR_LEN-1:0] npc_if;
-    reg [`ADDR_LEN-1:0] npc;
     reg [`ADDR_LEN-1:0] pc_if;
     reg [`INSN_LEN-1:0] inst1_if;
     //reg [`INSN_LEN-1:0] inst2_if;
@@ -34,22 +37,22 @@ module IFUnit (
    
    always @ (posedge clk_i) begin
       if (reset_i) begin
-	 npc <= `ENTRY_POINT;
+	 pc_o <= `ENTRY_POINT;
 //     end else if (prmiss) begin
-//	 pc_i <= jmpaddr;
+//	 pc_o <= jmpaddr;
       end else if (stall_IF) begin
-	 npc <= pc_i;
+	 pc_o <= pc_o;
       end else begin
-	 npc <= pc_i+4;
+	 pc_o <= npc;
       end
    end
 
    PipelineIF pipeline_if(
 		       .clk_i(clk_i),
 		       .reset_i(reset_i),
-		       .pc_i(pc_i),
-		       .npc_o(npc_o),
-		       .inst1_o(inst1_o),
+	   	       .pc_i(pc_o),
+		       .npc_o(npc),
+		       .inst1_o(inst1),
 //		       .inst2_o(inst2_o),
 //		       .invalid2_o(invalid2_pipe_o),
 		       .idata_i(idata_i)
@@ -65,9 +68,9 @@ module IFUnit (
 //	 inv2_if <= 1;
 	 
       end else if (~stall_IF) begin
-	 npc_if <= npc_o;
-	 pc_if <= pc_i;
-	 inst1_if <= inst1_o;
+	 npc_if <= npc;
+	 pc_if <= pc_o;
+	 inst1_if <= inst1;
 //	 inst2_if <= inst2_o;
 //	 inv1_if <= 0;
 //	 inv2_if <= invalid2_pipe_o;
