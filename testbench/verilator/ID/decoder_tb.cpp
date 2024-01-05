18bb3b2462cb758ef2b32bf44cc2be6d33c88e6e
Schedinger <58102563+Schedinger@users.noreply.github.com>
2024-01-05 14:32:14 +0800

    Update decoder_tb.cpp

--- testbench/verilator/ID/decoder_tb.cpp
+++ testbench/verilator/ID/decoder_tb.cpp
@@ -23,7 +23,7 @@ int main(int argc, char **argv, char **env) {
     auto m_trace = std::make_shared<VerilatedVcdC>();
     dut->trace(m_trace.get(), 99);
 
-    m_trace->open("imm_gen.vcd");
+    m_trace->open("decoder.vcd");
 
     while (sim_time < MAX_SIM_TIME) {
         dut->eval();
@@ -35,7 +35,12 @@ int main(int argc, char **argv, char **env) {
         }
         if (sim_time == 6) {
             // TODO: you should write some assert on this
-            std::cout << "imm_gen Test 1 Pass!" << std::endl;
+            assert(dut->imm_type_o==IMM_U);
+            assert(dut->rd_o==0x05);
+            assert(dut->uses_rs1_o==1'b0);
+            assert(dut->src_a_sel_o==SRC_A_ZERO);
+            assert(dut->wr_reg_o==1'b1);
+            std::cout << "decoder Test 1 Pass!" << std::endl;
         }
 
         /* 80000024:	374000ef          	jal	ra,80000398 <halt> */
@@ -45,7 +50,14 @@ int main(int argc, char **argv, char **env) {
         }
         if (sim_time == 8) {
             // TODO: you should write some assert on this
-            std::cout << "imm_gen Test 2 Pass!" << std::endl;
+            assert(dut->imm_type_o==IMM_J);
+            assert(dut->rd_o==0x01);
+            assert(dut->uses_rs1_o==1'b0);
+            assert(dut->src_a_sel_o==SRC_A_PC);
+            assert(dut->src_b_sel_o==SRC_B_FOUR);
+            assert(dut->wr_reg_o==1'b1);
+            assert(dut->rs_ent_o==RS_ENT_JAL);
+            std::cout << "decoder Test 2 Pass!" << std::endl;
         }
         /* 80000034:	00082883          	lw	a7,0(a6) */
         // type:I
@@ -54,7 +66,19 @@ int main(int argc, char **argv, char **env) {
         }
         if (sim_time == 10) {
             // TODO: you should write some assert on this
-            std::cout << "imm_gen Test 3 Pass!" << std::endl;
+            assert(dut->imm_type_o==IMM_I);
+            assert(dut->rs1_o==0x10);
+            assert(dut->rd_o==0x11);
+            assert(dut->src_a_sel_o==SRC_A_RS1);
+            assert(dut->src_b_sel_o==SRC_B_IMM);
+            assert(dut->wr_reg_o==1'b1);
+            assert(dut->uses_rs1_o==1'b1);
+            assert(dut->uses_rs2_o==1'b0);
+            assert(dut->illegal_instruction_o==1'b0);
+            assert(dut->rs_ent_o==RS_ENT_LDST);
+            assert(dut->dmem_size_o==0x2);
+            assert(dut->dmem_type_o==0x2);
+            std::cout << "decoder Test 3 Pass!" << std::endl;
         }
         /* 80000074:	00f82023          	sw	a5,0(a6) */
         // type:S
@@ -63,7 +87,19 @@ int main(int argc, char **argv, char **env) {
         }
         if (sim_time == 12) {
             // TODO: you should write some assert on this
-            std::cout << "imm_gen Test 4 Pass!" << std::endl;
+            assert(dut->imm_type_o==IMM_S);
+            assert(dut->rs1_o==0x10);
+            assert(dut->rs2_o==0x0f);
+            assert(dut->src_a_sel_o==SRC_A_RS1);
+            assert(dut->src_b_sel_o==SRC_B_IMM);
+            assert(dut->wr_reg_o==1'b0);
+            assert(dut->uses_rs1_o==1'b1);
+            assert(dut->uses_rs2_o==1'b1);
+            assert(dut->illegal_instruction_o==1'b0);
+            assert(dut->rs_ent_o==RS_ENT_LDST);
+            assert(dut->dmem_size_o==0x2);
+            assert(dut->dmem_type_o==0x2);
+            std::cout << "decoder Test 4 Pass!" << std::endl;
         }
         sim_time++;
     }
