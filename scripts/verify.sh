0cc24dd4db5ddbffa4586931368974b076d2d8d8
kuangjux <18630816527@163.com>
2024-01-08 10:48:34 +0800

    feat(ci): Add SW unit tests.

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -1,11 +1,15 @@
-# Stage 1: Run the pipeline stage tests.
-make sim STAGE=SW
-make sim STAGE=ROB
-make sim STAGE=DP
-
-# Stage 2: Run the unit tests.
+# Stage 1: Run the unit tests.
 make -C testbench/verilator/DP sim MODULE=SRCOPRMANAGER
 make -C testbench/verilator/DP sim MODULE=ARF
 make -C testbench/verilator/DP sim MODULE=RRF
 make -C testbench/verilator/DP sim MODULE=RRF_ALLO
+
+make -C testbench/verilator/SW sim MODULE=AllocateUnit
+make -C testbench/verilator/SW sim MODULE=RSAluEntry
+make -C testbench/verilator/SW sim MODULE=RSAlu
+
+# Stage 2: Run the pipeline stage tests.
+make sim STAGE=SW
+make sim STAGE=ROB
+make sim STAGE=DP
 
