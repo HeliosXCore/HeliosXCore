2904f397c39ea25a9bf6dc73b62c56868dc312bb
jklincn <jklincn@outlook.com>
2024-01-10 11:36:13 +0800

    chore(CI): optimize verify speed

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -1,18 +1,17 @@
 # Stage 1: Run the unit tests.
-make -C testbench/verilator/DP sim MODULE=SRCOPRMANAGER
-make -C testbench/verilator/DP sim MODULE=ARF
-make -C testbench/verilator/DP sim MODULE=RRF
-make -C testbench/verilator/DP sim MODULE=RRF_ALLO
+make -j -C testbench/verilator/DP sim MODULE=SRCOPRMANAGER
+make -j -C testbench/verilator/DP sim MODULE=ARF
+make -j -C testbench/verilator/DP sim MODULE=RRF
+make -j -C testbench/verilator/DP sim MODULE=RRF_ALLO
 
-make -C testbench/verilator/SW sim MODULE=AllocateUnit
-make -C testbench/verilator/SW sim MODULE=RSAluEntry
-make -C testbench/verilator/SW sim MODULE=RSAlu
+make -j -C testbench/verilator/SW sim MODULE=AllocateUnit
+make -j -C testbench/verilator/SW sim MODULE=RSAluEntry
+make -j -C testbench/verilator/SW sim MODULE=RSAlu
 
 make -C testbench/verilator/EX sim MODULE=StoreBuffer
 
 # Stage 2: Run the pipeline stage tests.
-make sim STAGE=SW
-make sim STAGE=ROB
-make sim STAGE=DP
-make sim STAGE=EX
+make -j sim STAGE=SW
+make -j sim STAGE=ROB
+make -j sim STAGE=DP
 
