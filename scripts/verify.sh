e69ee1f5ada2f151705d558d955379336c4205c3
ChengXiang Qi <18630816527@163.com>
2024-01-10 12:17:55 +0800

    chore(CI): optimize verify speed

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -8,7 +8,7 @@ make -j -C testbench/verilator/SW sim MODULE=AllocateUnit
 make -j -C testbench/verilator/SW sim MODULE=RSAluEntry
 make -j -C testbench/verilator/SW sim MODULE=RSAlu
 
-make -C testbench/verilator/EX sim MODULE=StoreBuffer
+make -j -C testbench/verilator/EX sim MODULE=StoreBuffer
 
 # Stage 2: Run the pipeline stage tests.
 make -j sim STAGE=SW
