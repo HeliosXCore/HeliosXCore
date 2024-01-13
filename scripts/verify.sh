67df8629383feb7b8798b3c14dc4f5f6ad472514
Schedinger <58102563+Schedinger@users.noreply.github.com>
2024-01-13 15:53:54 +0800

    Update verify.sh

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -10,10 +10,10 @@ make -j -C testbench/verilator/SW sim MODULE=RSAlu
 
 make -j -C testbench/verilator/EX sim MODULE=StoreBuffer
 
-make -j -c testbench/verilator/ID sim MODULE=IDUnit
+//make -j -c testbench/verilator/ID sim MODULE=Decoder
 
 # Stage 2: Run the pipeline stage tests.
 make -j sim STAGE=SW
 make -j sim STAGE=ROB
 make -j sim STAGE=DP
-make -j sim STAGE=ID
+//make -j sim STAGE=ID
