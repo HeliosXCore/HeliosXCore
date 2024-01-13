2f4f979e8e2c784072e3761a90dd47b04ad0ad65
Schedinger <58102563+Schedinger@users.noreply.github.com>
2024-01-13 12:33:53 +0800

    Update idunit_tb.cpp

--- testbench/verilator/ID/idunit_tb.cpp
+++ testbench/verilator/ID/idunit_tb.cpp
@@ -1,104 +1,156 @@
-#include <cstdlib>
-#include <memory>
-#include <verilated.h>
-#include <verilated_vcd_c.h>
-
-#include <stdlib.h>
-#include <assert.h>
+#include "fmt/core.h"
+#include "verilator_tb.hpp"
+#include "VIDUnit.h"
+#include "VIDUnit___024root.h"
+#include "error_handler.hpp"
 #include <iostream>
-
-#include "obj_dir/VIDUnit.h"
+#include <verilated.h>
 #include "decoder.hpp"
 
-#define MAX_SIM_TIME 300
-#define VERIF_START_TIME 7
 vluint64_t sim_time = 0;
 
-int main(int argc, char **argv, char **env) {
-    srand(time(NULL));
-    Verilated::commandArgs(argc, argv);
-    auto dut = std::make_shared<VIDUnit>();
+template <>
+void VerilatorTb<VIDUnit>::initialize_signal() {
+    dut->clk_i = 0;
+    dut->reset_i = 1;
 
-    Verilated::traceEverOn(true);
-    auto m_trace = std::make_shared<VerilatedVcdC>();
-    dut->trace(m_trace.get(), 99);
+    dut->inst1_i = 0;
+};
 
-    m_trace->open("idunit.vcd");
+class VIDUnitTb : public VerilatorTb<VIDUnit> {
+   public:
+    VIDUnitTb(uint64_t clock, uint64_t start_time, uint64_t end_time)
+        : VerilatorTb<VIDUnit>(clock, start_time, end_time) {}
 
-    while (sim_time < MAX_SIM_TIME) {
-        dut->eval();
-
-        if (sim_time == 5) {
+    void test1_input() {
+        if (sim_time == 50) {
+            dut->reset_i = 0;
             // type:U
             /* 0x800002b7,  // lui t0,0x80000 */
             dut->inst1_i = 0x800002b7;
         }
-        if (sim_time == 6) {
-            assert(dut->imm_type_1_o==IMM_U);
-            assert(dut->rd_1_o==0x05);
-            assert(dut->uses_rs1_1_o==0);
-            assert(dut->src_a_sel_1_o==SRC_A_ZERO);
-            assert(dut->wr_reg_1_o==1);
-            std::cout << "IDUnit Test 1 Pass!" << std::endl;
-        }
-
-        /* 80000024:	374000ef          	jal	ra,80000398 <halt> */
-        // type: J
-        if (sim_time == 7) {
-            dut->inst1_i = 0x374000ef;
-        }
-        if (sim_time == 8) {
-            assert(dut->imm_type_1_o==IMM_I);
-            assert(dut->rd_1_o==0x01);
-            assert(dut->uses_rs1_1_o==0);
-            assert(dut->src_a_sel_1_o==SRC_A_PC);
-            assert(dut->src_b_sel_1_o==SRC_B_FOUR);
-            assert(dut->wr_reg_1_o==1);
-            assert(dut->rs_ent_1_o==RS_ENT_JAL);
-            std::cout << "IDUnit Test 2 Pass!" << std::endl;
-        }
-        /* 80000034:	00082883          	lw	a7,0(a6) */
-        // type:I
-        if (sim_time == 9) {
-            dut->inst1_i = 0x00082883;
-        }
-        if (sim_time == 10) {
-            assert(dut->imm_type_1_o==IMM_I);
-            assert(dut->rs1_1_o==0x10);
-            assert(dut->rd_1_o==0x11);
-            assert(dut->src_a_sel_1_o==SRC_A_RS1);
-            assert(dut->src_b_sel_1_o==SRC_B_IMM);
-            assert(dut->wr_reg_1_o==1);
-            assert(dut->uses_rs1_1_o==1);
-            assert(dut->uses_rs2_1_o==0);
-            assert(dut->illegal_instruction_1_o==0);
-            assert(dut->rs_ent_1_o==RS_ENT_LDST);
-            assert(dut->dmem_size_1_o==0x2);
-            assert(dut->dmem_type_1_o==0x2);
-            std::cout << "IDUnit Test 3 Pass!" << std::endl;
-        }
-        /* 80000074:	00f82023          	sw	a5,0(a6) */
-        // type:S
-        if (sim_time == 11) {
-            dut->inst1_i = 0x00f82023;
-        }
-        if (sim_time == 12) {
-            assert(dut->imm_type_1_o==IMM_S);
-            assert(dut->rs1_1_o==0x10);
-            assert(dut->rs2_1_o==0x0f);
-            assert(dut->src_a_sel_1_o==SRC_A_RS1);
-            assert(dut->src_b_sel_1_o==SRC_B_IMM);
-            assert(dut->wr_reg_1_o==0);
-            assert(dut->uses_rs1_1_o==1);
-            assert(dut->uses_rs2_1_o==1);
-            assert(dut->illegal_instruction_1_o==0);
-            assert(dut->rs_ent_1_o==RS_ENT_LDST);
-            assert(dut->dmem_size_1_o==0x2);
-            assert(dut->dmem_type_1_o==0x2);
-            std::cout << "IDUnit Test 4 Pass!" << std::endl;
-        }
-        sim_time++;
     }
 
-    m_trace->dump(sim_time);
+    void test1_verify() {
+        if (sim_time == 55) {
+            ASSERT(dut->imm_type_1_o == IMM_U);
+            ASSERT(dut->rs1_1_o == inst1_i[19:15]);
+            ASSERT(dut->rs2_1_o == inst1_i[24:20]);
+            ASSERT(dut->rd_1_o == 0x05);
+            ASSERT(dut->src_a_sel_1_o == SRC_A_ZERO);
+            ASSERT(dut->src_b_sel_1_o == SRC_B_IMM);
+            ASSERT(dut->wr_reg_1_o == 1);
+            ASSERT(dut->uses_rs1_1_o == 0);
+            ASSERT(dut->uses_rs2_1_o == 0);
+            ASSERT(dut->illegal_instruction_1_o == 0);
+            ASSERT(dut->alu_op_1_o == ALU_OP_ADD);
+            ASSERT(dut->rs_ent_1_o == RS_ENT_ALU);
+            
+            fmt::println("IDUnit test1 passed!");
+        }
+    }
+
+    void test2_input() {
+        if (sim_time == 60) {
+        // type: J
+        /* 80000024:	374000ef          	jal	ra,80000398 <halt> */
+            dut->inst1_i = 0x374000ef;
+        }
+    }
+
+    void test2_verify() {
+        if (sim_time == 65) {
+            ASSERT(dut->imm_type_1_o == IMM_J);
+            ASSERT(dut->rd_1_o == 0x01);
+            ASSERT(dut->src_a_sel_1_o == SRC_A_PC);
+            ASSERT(dut->src_b_sel_1_o == SRC_B_FOUR);
+            ASSERT(dut->wr_reg_1_o == 1);
+            ASSERT(dut->uses_rs1_1_o == 0);
+            ASSERT(dut->uses_rs2_1_o == 0);
+            ASSERT(dut->illegal_instruction_1_o == 0);
+            ASSERT(dut->alu_op_1_o == ALU_OP_ADD);
+            ASSERT(dut->rs_ent_1_o == RS_ENT_JAL);
+
+            fmt::println("IDUnit test2 passed!");
+        }
+    }
+
+    void test3_input() {
+        if (sim_time == 70) {
+        // type:I
+        /* 80000034:	00082883          	lw	a7,0(a6) */
+            dut->inst1_i = 0x00082883;
+        }
+    }
+
+    void test3_verify() {
+        if (sim_time == 75) {
+            ASSERT(dut->imm_type_o == IMM_I);
+            ASSERT(dut->rs1_o == 0x10);
+            ASSERT(dut->rd_o == 0x11);
+            ASSERT(dut->src_a_sel_o == SRC_A_RS1);
+            ASSERT(dut->src_b_sel_o == SRC_B_IMM);
+            ASSERT(dut->wr_reg_o == 1);
+            ASSERT(dut->uses_rs1_o == 1);
+            ASSERT(dut->uses_rs2_o == 0);
+            ASSERT(dut->illegal_instruction_o == 0);
+            ASSERT(dut->rs_ent_o == RS_ENT_LDST);
+            ASSERT(dut->alu_op_1_o == ALU_OP_ADD);
+            ASSERT(dut->dmem_size_o == 0x2);
+            ASSERT(dut->dmem_type_o == 0x2);
+
+            fmt::println("IDUnit test3 passed!");
+        }
+    }
+
+    void test4_input() {
+        if (sim_time == 80) {
+        // type:S
+        /* 80000074:	00f82023          	sw	a5,0(a6) */
+            dut->inst1_i = 0x00f82023;
+        }
+    }
+
+    void test4_verify() {
+        if (sim_time == 85) {
+            ASSERT(dut->imm_type_o == IMM_S);
+            ASSERT(dut->rs1_o == 0x10);
+            ASSERT(dut->rs2_o == 0x0f);
+            ASSERT(dut->src_a_sel_o == SRC_A_RS1);
+            ASSERT(dut->src_b_sel_o == SRC_B_IMM);
+            ASSERT(dut->wr_reg_o == 0);
+            ASSERT(dut->uses_rs1_o == 1);
+            ASSERT(dut->uses_rs2_o == 1);
+            ASSERT(dut->illegal_instruction_o == 0);
+            ASSERT(dut->alu_op_1_o == ALU_OP_ADD);
+            ASSERT(dut->rs_ent_o == RS_ENT_LDST);
+            ASSERT(dut->dmem_size_o == 0x2);
+            ASSERT(dut->dmem_type_o == 0x2);
+
+            fmt::println("IDUnit test4 passed!");
+        }
+    }
+
+    void input() {
+        test1_input();
+        test2_input();
+        test3_input();
+        test4_input();
+    }
+
+    void verify_dut() {
+        test1_verify();
+        test2_verify();
+        test3_verify();
+        test4_verify();
+    }
+};
+
+int main(int argc, char **argv, char **env) {
+    srand(time(NULL));
+    Verilated::commandArgs(argc, argv);
+
+    std::shared_ptr<VIDUnitTb> tb = std::make_shared<VIDUnitTb>(5, 50, 1000);
+
+    tb->run("IDUnit.vcd");
 }
