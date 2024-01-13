5ff4f0a29774294640084fecff8ab8937b19eadc
Schedinger <58102563+Schedinger@users.noreply.github.com>
2024-01-13 10:08:10 +0800

    Update ImmDecoder.v

--- rtl/core/ID/ImmDecoder.v
+++ rtl/core/ID/ImmDecoder.v
@@ -3,19 +3,14 @@
 module ImmDecoder (
     input wire [`INSN_LEN-1:0] inst,
     input wire [`IMM_TYPE_WIDTH-1:0] imm_type,
-    output reg [`DATA_LEN-1:0] imm
+    output wire [`DATA_LEN-1:0] imm
 );
 
-  always @(*) begin
-    case (imm_type)
-      `IMM_I:  imm = {{21{inst[31]}}, inst[30:25], inst[24:21], inst[20]};
-      `IMM_S:  imm = {{21{inst[31]}}, inst[30:25], inst[11:8], inst[7]};
-      `IMM_U:  imm = {inst[31], inst[30:20], inst[19:12], 12'b0};
-      `IMM_J:  imm = {{12{inst[31]}}, inst[19:12], inst[20], inst[30:25], inst[24:21], 1'b0};
-      default: imm = {{21{inst[31]}}, inst[30:25], inst[24:21], inst[20]};
-    endcase  // case (imm_type)
-  end
+assign imm = (imm_type == `IMM_I) ? {{21{inst[31]}}, inst[30:25], inst[24:21], inst[20]} :
+             (imm_type == `IMM_S) ? {{21{inst[31]}}, inst[30:25], inst[11:8], inst[7]} :
+             (imm_type == `IMM_U) ? {inst[31], inst[30:20], inst[19:12], 12'b0} :
+             (imm_type == `IMM_J) ? {{12{inst[31]}}, inst[19:12], inst[20], inst[30:25], inst[24:21], 1'b0} :
+             {{21{inst[31]}}, inst[30:25], inst[24:21], inst[20]};
 
 endmodule
-
 `default_nettype wire
