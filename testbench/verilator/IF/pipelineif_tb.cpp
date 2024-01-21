50e8580e4544134b1d16e990dc5fbd1fd2254335
Schedinger <58102563+Schedinger@users.noreply.github.com>
2024-01-21 13:26:47 +0800

    Update pipelineif_tb.cpp

--- testbench/verilator/IF/pipelineif_tb.cpp
+++ testbench/verilator/IF/pipelineif_tb.cpp
@@ -1,86 +1,94 @@
+#include "fmt/core.h"
 #include "verilator_tb.hpp"
-#include "VPipelineIF.h"
+#include "VPipelinIF.h"
 #include "VPipelineIF___024root.h"
 #include "error_handler.hpp"
 #include <iostream>
 #include <verilated.h>
 
-
-#define MAX_SIM_TIME 300
 vluint64_t sim_time = 0;
 
-
-template<>
-void  VerilatorTb<VPipelineIF>::initialize_signal(){
+template <>
+void VerilatorTb<VPipelineIF>::initialize_signal() {
     dut->clk_i = 0;
-    dut->reset_i = 1; 
+    dut->reset_i = 1;
     dut->pc_i = 0;
     dut->idata_i = 0;
 };
 
-
-
 class VPipelineIFTb : public VerilatorTb<VPipelineIF> {
-    public:
-        VPipelineIFTb(uint64_t clock, uint64_t start_time, uint64_t end_time)
-            : VerilatorTb<VSingleInstROB>(clock, start_time, end_time) {}
-        
-        void if_test(){
-            //int pc = 0x000f4048;
-            while(sim_time < MAX_SIM_TIME){
-                dut->eval();
+   public:
+    VPipelineIFTb(uint64_t clock, uint64_t start_time, uint64_t end_time)
+        : VerilatorTb<VPipelineIF>(clock, start_time, end_time) {}
 
-                if(sim_time == 5){
-                    dut->pc_i = 0x000f4048;
-                    dut->idata_i = 0x0000000526300100ffdff06f00202423;
-                }
-                if(sim_time == 6){
-                    assert(dut->npc_o==0x000f404c);
-                    assert(dut->inst1_o==0x26300100);
-                    std::cout << "PipelineIF Test 1 Pass!" << std::endl;
-                }
+    void test1_input() {
+        if (sim_time == 50) {
+            dut->reset_i = 0;
 
-                if(sim_time == 7){
-                    dut->pc_i = 0x000f2620;
-                    dut->idata_i = 0x0000000526300100ffdff06fc2804365;
-                }
-                if(sim_time == 8){
-                    assert(dut->npc_o==0x000f2624);
-                    assert(dut->inst1_o==0xc2804365);
-                    std::cout << "PipelineIF Test 2 Pass!" << std::endl;
-                }
-
-                if(sim_time == 9){
-                    dut->pc_i = 0x000f0717;
-                    dut->idata_i = 0x0000000526300100ffdff06f00202423;
-                }
-                if(sim_time == 10){
-                    assert(dut->npc_o==0x000f071b);
-                    assert(dut->inst1_o==0xffdff06f);
-                    std::cout << "PipelineIF Test 3 Pass!" << std::endl;
-                }
-                sim_time++;
-
-            }
-
+            dut->pc_i = 0x000f4048;
+            dut->idata_i = 0x0000000526300100ffdff06f00202423;
         }
-        // int count = 1;
-        void verify_dut() {
-            if_test();
-            // fmt::println("count execute:{} times", count);
-            // count++;
-            // fmt::println("Full Test Pass!");
+    }
+
+    void test1_verify() {
+        if (sim_time == 55) {
+            ASSERT(dut->npc_o==0x000f404c,"npc error");
+            ASSERT(dut->inst1_o==0x26300100,"inst1 error");
+            
+            fmt::println("PipelineIF test1 passed!");
         }
+    }
+
+    void test2_input() {
+        if (sim_time == 60) {
+            dut->pc_i = 0x000f2620;
+            dut->idata_i = 0x0000000526300100ffdff06fc2804365;
+        }
+    }
+
+    void test2_verify() {
+        if (sim_time == 65) {
+            ASSERT(dut->npc_o==0x000f2624,"npc error");
+            ASSERT(dut->inst1_o==0xc2804365,"inst1 error");
+
+            fmt::println("PipelineIF test2 passed!");
+        }
+    }
+
+    void test3_input() {
+        if (sim_time == 70) {
+            dut->pc_i = 0x000f0717;
+            dut->idata_i = 0x0000000526300100ffdff06f00202423;
+        }
+    }
+
+    void test3_verify() {
+        if (sim_time == 75) {
+            ASSERT(dut->npc_o==0x000f071b,"npc error");
+            ASSERT(dut->inst1_o==0xffdff06f,"inst1 error");
+
+            fmt::println("PipelineIF test3 passed!");
+        }
+    }
+
+    void input() {
+        test1_input();
+        test2_input();
+        test3_input();
+    }
+
+    void verify_dut() {
+        test1_verify();
+        test2_verify();
+        test3_verify();
+    }
 };
-
-
-
 
 int main(int argc, char **argv, char **env) {
     srand(time(NULL));
     Verilated::commandArgs(argc, argv);
 
-    std::shared_ptr<VPipelineIFTb> tb = std::make_shared<VPipelineIFTb>(1, 0, 200);
+    std::shared_ptr<VPipelineIFTb> tb = std::make_shared<VPipelineIFTb>(5, 50, 1000);
 
     tb->run("PipelineIF.vcd");
 }
