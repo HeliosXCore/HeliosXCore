4ddc6351a326fca4f123e70cfae7cfe95fdba482
Schedinger <58102563+Schedinger@users.noreply.github.com>
2024-01-21 13:24:45 +0800

    Update PipelineIF.v

--- rtl/core/IF/PipelineIF.v
+++ rtl/core/IF/PipelineIF.v
@@ -1,57 +1,36 @@
 `include "consts/Consts.vh"
 `default_nettype none
-module PipelineIF
-  (
-   input wire 			  clk_i,
-   input wire 			  reset_i,
-   input wire [`ADDR_LEN-1:0] 	  pc_i,
-   input wire [4*`INSN_LEN-1:0]   idata_i,
+module PipelineIF(
+	input wire 			  clk_i,
+    input wire 			  reset_i,
+    input wire [`ADDR_LEN-1:0] 	  pc_i,
+    input wire [4*`INSN_LEN-1:0]   idata_i,
 	  
-   output wire [`ADDR_LEN-1:0] 	  npc_o,
-   output wire [`INSN_LEN-1:0] 	  inst1_o
-   );
+    output wire [`ADDR_LEN-1:0] 	  npc_o,
+    output wire [`INSN_LEN-1:0] 	  inst1_o
+);
 
-   assign npc_o = pc_i + 4;
+	assign npc_o = pc_i + 4;
 
-   select_logic sellog(
-	   .sel_i(pc_i[3:2]),
-	   .idata_i(idata_i),
-	   .inst1_o(inst1_o)
-   );
+    Selector selector(
+		.sel_i(pc_i[3:2]),
+	    .idata_i(idata_i),
+	    .inst1_o(inst1_o)
+	);
 endmodule
 
 
-module select_logic
-  (
-	  input wire [1:0] 		sel_i,
-	  input wire [4*`INSN_LEN-1:0] idata_i,
-	  output reg [`INSN_LEN-1:0] 	inst1_o
-   );
+module Selector(
+	input wire [1:0] 		sel_i,
+	input wire [4*`INSN_LEN-1:0] idata_i,
+	output wire [`INSN_LEN-1:0] 	inst1_o
+);
 
-   always @ (*) begin
-      inst1_o = `INSN_LEN'h0;
-//      inst2_o = `INSN_LEN'h0;
-      
-	   case(sel_i)
-	2'b00 : begin
-	   inst1_o = idata_i[31:0];
-//	   inst2_o = idata_i[63:32];
-	end
-	2'b01 : begin
-	   inst1_o = idata_i[63:32];
-//	   inst2_o = idata_i[95:64];
-	end
-	2'b10 : begin
-	   inst1_o = idata_i[95:64];
-//	   inst2_o = idata_i[127:96];
-	end
-	2'b11 : begin
-	   inst1_o = idata_i[127:96];
-//	   inst2_o = idata_i[31:0];
-	end
-      endcase // case (sel)
-   end
+	assign inst1_o = (sel_i == 2'b00) ? idata_i[31:0] :
+					 (sel_i == 2'b01) ? idata_i[63:32] :
+				 	 (sel_i == 2'b10) ? idata_i[95:64] :
+				 	 (sel_i == 2'b11) ? idata_i[127:96] :
+				 	 `INSN_LEN'h0;
    
-endmodule // select_logic
-
+endmodule
 `default_nettype wire
