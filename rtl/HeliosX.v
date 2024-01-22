4b10f9f0b5386c7de1509c28952fdf907bccf680
jklincn <jklincn@outlook.com>
2024-01-22 12:24:11 +0800

    feat(pipeline): add EX signals to pipeline

--- rtl/HeliosX.v
+++ rtl/HeliosX.v
@@ -77,7 +77,7 @@ module HeliosX (
     wire [    `DATA_LEN-1:0] dp_op_2_1;
     wire [    `DATA_LEN-1:0] dp_op_2_2;
 
-    wire                     dp_valid_1_1;
+    wire                     dp_valid_1_1;wire ,
     wire                     dp_valid_1_2;
     wire                     dp_valid_2_1;
     wire                     dp_valid_2_2;
@@ -97,36 +97,115 @@ module HeliosX (
     wire                     stall_dp;
     wire                     kill_dp;
 
-    // 执行前递的信号
-    wire [    `DATA_LEN-1:0] exe_result_1;
-    wire [    `DATA_LEN-1:0] exe_result_2;
-    wire [    `DATA_LEN-1:0] exe_result_3;
-    wire [    `DATA_LEN-1:0] exe_result_4;
-    wire [    `DATA_LEN-1:0] exe_result_5;
-    wire [     `RRF_SEL-1:0] exe_result_1_dst;
-    wire [     `RRF_SEL-1:0] exe_result_2_dst;
-    wire [     `RRF_SEL-1:0] exe_result_3_dst;
-    wire [     `RRF_SEL-1:0] exe_result_4_dst;
-    wire [     `RRF_SEL-1:0] exe_result_5_dst;
 
-    wire [    `DATA_LEN-1:0] exe_alu_op_1;
-    wire [    `DATA_LEN-1:0] exe_alu_op_2;
-    wire [    `ADDR_LEN-1:0] exe_alu_pc;
-    wire [    `DATA_LEN-1:0] exe_alu_imm;
-    wire [     `RRF_SEL-1:0] exe_alu_rrf_tag;
-    wire                     exe_alu_dst_val;
-    wire [`ALU_OP_WIDTH-1:0] exe_alu_op;
-    wire [ `ALU_ENT_NUM-1:0] exe_alu_ready;
-    wire                     exe_alu_issue;
+    // ------------ EX input -------------
+    // alu
+    wire ex_alu_issue;
+    wire ex_alu_if_write_rrf;
+    wire [`RRF_SEL-1:0] ex_alu_rrf_tag;
+    wire [`ADDR_LEN-1:0] ex_alu_pc;
+    wire [`DATA_LEN-1:0] ex_alu_imm;
+    wire [`ALU_OP_WIDTH-1:0] ex_alu_alu_op;
+    wire [`DATA_LEN-1:0] ex_alu_src1;
+    wire [`SRC_A_SEL_WIDTH-1:0] ex_alu_src_a_select;
+    wire [`DATA_LEN-1:0] ex_alu_src2;
+    wire [`SRC_B_SEL_WIDTH-1:0] ex_alu_src_b_select;
+    // branch
+    wire ex_branch_issue;
+    wire ex_branch_if_write_rrf;
+    wire [`RRF_SEL-1:0] ex_branch_rrf_tag;
+    wire [`ADDR_LEN-1:0] ex_branch_pc;
+    wire [`DATA_LEN-1:0] ex_branch_imm;
+    wire [`ALU_OP_WIDTH-1:0] ex_branch_alu_op;
+    wire [`DATA_LEN-1:0] ex_branch_src1;
+    wire [`DATA_LEN-1:0] ex_branch_src2;
+    wire [`OPCODE_LEN-1:0] ex_branch_opcode;
+    // memaccess
+    wire ex_mem_access_issue;
+    wire [`DATA_LEN-1:0] ex_mem_access_src1;
+    wire [`DATA_LEN-1:0] ex_mem_access_src2;
+    wire [`DATA_LEN-1:0] ex_mem_access_imm;
+    wire ex_mem_access_if_write_rrf;
+    wire ex_mem_access_complete;
+    wire [`DATA_LEN-1:0] ex_mem_access_load_data_from_data_memory;
+    wire [`RRF_SEL-1:0] ex_mem_access_rrf_tag;
 
-    wire [    `DATA_LEN-1:0] exe_mem_op_1;
-    wire [    `DATA_LEN-1:0] exe_mem_op_2;
-    wire [    `ADDR_LEN-1:0] exe_mem_pc;
-    wire [    `DATA_LEN-1:0] exe_mem_imm;
-    wire [     `RRF_SEL-1:0] exe_mem_rrf_tag;
-    wire                     exe_mem_dst_val;
-    wire [`LDST_ENT_NUM-1:0] exe_mem_ready;
-    wire                     exe_mem_issue_o;
+    // ------------ EX output ------------
+    // alu
+    wire [`DATA_LEN-1:0] ex_alu_result;
+    wire [`RRF_SEL-1:0] ex_alu_rrf_tag;
+    wire ex_alu_rob_we;
+    wire alu_rrf_we;
+    // branch
+    wire [`DATA_LEN-1:0] ex_branch_result;
+    wire [`RRF_SEL-1:0] ex_branch_rrf_tag;
+    wire ex_branch_rob_we;
+    wire ex_branch_rrf_we;
+    wire [`ADDR_LEN-1:0] ex_branch_jump_result;
+    wire [`ADDR_LEN-1:0] ex_branch_jump_addr;
+    wire ex_branch_if_jump;
+    // memaccess
+    wire ex_mem_access_rrf_we;
+    wire ex_mem_access_rob_we;
+    wire [`ADDR_LEN-1:0] ex_mem_access_load_address;
+    wire ex_mem_access_store_buffer_mem_we;
+    wire [`ADDR_LEN-1:0] ex_mem_access_store_buffer_write_address;
+    wire [`DATA_LEN-1:0] ex_mem_access_store_buffer_write_data;
+    wire [`DATA_LEN-1:0] ex_mem_access_load_data;
+    wire [`RRF_SEL-1:0] ex_mem_access_rrf_tag;
+
+    // ------------------------------------------------- EX -------------------------------------------------------
+    ExUnit ex_unit (
+    .clk_i                                    ( clk_i                                     ),
+    .reset_i                                  ( reset_i                                   ),
+    .alu_issue_i                              ( ex_alu_issue                               ),
+    .alu_if_write_rrf_i                       ( ex_alu_if_write_rrf                        ),
+    .alu_rrf_tag_i                            ( ex_alu_rrf_tag                             ),
+    .alu_pc_i                                 ( ex_alu_pc                                  ),
+    .alu_imm_i                                ( ex_alu_imm                                 ),
+    .alu_alu_op_i                             ( ex_alu_alu_op                              ),
+    .alu_src1_i                               ( ex_alu_src1                                ),
+    .alu_src_a_select_i                       ( ex_alu_src_a_select                        ),
+    .alu_src2_i                               ( ex_alu_src2                                ),
+    .alu_src_b_select_i                       ( ex_alu_src_b_select                        ),
+    .branch_issue_i                           ( ex_branch_issue                            ),
+    .branch_if_write_rrf_i                    ( ex_branch_if_write_rrf                     ),
+    .branch_rrf_tag_i                         ( ex_branch_rrf_tag                          ),
+    .branch_pc_i                              ( ex_branch_pc                               ),
+    .branch_imm_i                             ( ex_branch_imm                              ),
+    .branch_alu_op_i                          ( ex_branch_alu_op                           ),
+    .branch_src1_i                            ( ex_branch_src1                             ),
+    .branch_src2_i                            ( ex_branch_src2                             ),
+    .branch_opcode_i                          ( ex_branch_opcode                           ),
+    .mem_access_src1_i                        ( ex_mem_access_src1                         ),
+    .mem_access_src2_i                        ( ex_mem_access_src2                         ),
+    .mem_access_imm_i                         ( ex_mem_access_imm                          ),
+    .mem_access_if_write_rrf_i                ( ex_mem_access_if_write_rrf                 ),
+    .mem_access_issue_i                       ( ex_mem_access_issue                        ),
+    .mem_access_complete_i                    ( ex_mem_access_complete                     ),
+    .mem_access_load_data_from_data_memory_i  ( ex_mem_access_load_data_from_data_memory   ),
+    .mem_access_rrf_tag_i                     ( ex_mem_access_rrf_tag                      ),
+
+    .alu_result_o                             ( ex_alu_result                              ),
+    .alu_rrf_tag_o                            ( ex_alu_rrf_tag                             ),
+    .alu_rob_we_o                             ( ex_alu_rob_we                              ),
+    .alu_rrf_we_o                             ( ex_alu_rrf_we                              ),
+    .branch_result_o                          ( ex_branch_result                           ),
+    .branch_rrf_tag_o                         ( ex_branch_rrf_tag                          ),
+    .branch_rob_we_o                          ( ex_branch_rob_we                           ),
+    .branch_rrf_we_o                          ( ex_branch_rrf_we                           ),
+    .branch_jump_result_o                     ( ex_branch_jump_result                      ),
+    .branch_jump_addr_o                       ( ex_branch_jump_addr                        ),
+    .branch_if_jump_o                         ( ex_branch_if_jump                          ),
+    .mem_access_rrf_we_o                      ( ex_mem_access_rrf_we                       ),
+    .mem_access_rob_we_o                      ( ex_mem_access_rob_we                       ),
+    .mem_access_load_address_o                ( ex_mem_access_load_address                 ),
+    .mem_access_store_buffer_mem_we_o         ( ex_mem_access_store_buffer_mem_we          ),
+    .mem_access_store_buffer_write_address_o  ( ex_mem_access_store_buffer_write_address   ),
+    .mem_access_store_buffer_write_data_o     ( ex_mem_access_store_buffer_write_data      ),
+    .mem_access_load_data_o                   ( ex_mem_access_load_data                    ),
+    .mem_access_rrf_tag_o                     ( ex_mem_access_rrf_tag                      )
+);
 
 endmodule
 
