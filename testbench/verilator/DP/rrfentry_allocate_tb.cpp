a3e072954c7cabe9b322491d855693f0ce2e13cd
MrGeek-zrh <mrgeek-zrh@outlook.com>
2024-01-23 16:46:04 +0800

    fix(DP): fix CI error

--- testbench/verilator/DP/rrfentry_allocate_tb.cpp
+++ testbench/verilator/DP/rrfentry_allocate_tb.cpp
@@ -41,45 +41,47 @@ int main(int argc, char **argv, char **env) {
 
         if ((sim_time % 5) == 0) {
             dut->clk_i = !dut->clk_i;
+            if (dut->clk_i == 1) {
+                posedge_cnt++;
+            }
         }
         dut->eval();
 
         vluint64_t rrftag_rand_1;
         vluint64_t rrftag_rand_2;
         if (dut->clk_i == 1) {
-            posedge_cnt++;
-            if (posedge_cnt == 2) {
+            if (posedge_cnt == 2 && sim_time == 10) {
                 assert(dut->rrf_allocatable_o == 1);
                 assert(dut->freenum_o == 64);
                 assert(dut->rrfptr_o == 0);
                 assert(dut->nextrrfcyc_o == 0);
                 std::cout << "Rrf_alloc Test 1 Pass!" << std::endl;
-            } else if (posedge_cnt == 4) {
+            } else if (posedge_cnt == 4 && sim_time == 30) {
                 assert(dut->freenum_o == 62);
                 std::cout << "Rrf_alloc Test 2 Pass!" << std::endl;
 
                 dut->stall_dp_i = 1;
-            } else if (posedge_cnt >= 5 && posedge_cnt < 8) {
+            } else if (posedge_cnt >= 5 && posedge_cnt < 8 && sim_time == 40) {
                 assert(dut->freenum_o == 62);
                 std::cout << "Rrf_alloc Test 3 Pass!" << std::endl;
-            } else if (posedge_cnt == 8) {
+            } else if (posedge_cnt == 8 && sim_time == 70) {
                 dut->stall_dp_i = 0;
-            } else if (posedge_cnt == 9) {
+            } else if (posedge_cnt == 9 && sim_time == 80) {
                 assert(dut->freenum_o == 61);
                 std::cout << "Rrf_alloc Test 4 Pass!" << std::endl;
-            } else if (posedge_cnt == 10) {
+            } else if (posedge_cnt == 10 && sim_time == 90) {
                 assert(dut->freenum_o == 60);
                 std::cout << "Rrf_alloc Test 5 Pass!" << std::endl;
 
                 dut->stall_dp_i = 1;
                 dut->com_inst_num_i = 3;
-            } else if (posedge_cnt == 11) {
+            } else if (posedge_cnt == 11 && sim_time == 100) {
                 assert(dut->freenum_o == 63);
                 std::cout << "Rrf_alloc Test 6 Pass!" << std::endl;
 
                 dut->stall_dp_i = 0;
                 dut->com_inst_num_i = 0;
-            } else if (posedge_cnt == 12) {
+            } else if (posedge_cnt == 12 && sim_time == 110) {
                 assert(dut->freenum_o == 62);
                 std::cout << "Rrf_alloc Test 7 Pass!" << std::endl;
             }
