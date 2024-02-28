334d9bcbc74316267537f4a15533bf3e2b583665
jklincn <jklincn@outlook.com>
2024-02-28 14:46:01 +0800

    fix(EX_test): fix verify time

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -17,7 +17,7 @@ make -j -C testbench/verilator/SW sim MODULE=AllocateUnit
 make -j -C testbench/verilator/SW sim MODULE=RSAluEntry
 make -j -C testbench/verilator/SW sim MODULE=RSAlu
 # Execute Stage Modules
-# make -j -C testbench/verilator/EX sim MODULE=StoreBuffer
+make -j -C testbench/verilator/EX sim MODULE=StoreBuffer
 
 # Stage 2: Run the pipeline stage tests.
 make -j sim STAGE=IF
