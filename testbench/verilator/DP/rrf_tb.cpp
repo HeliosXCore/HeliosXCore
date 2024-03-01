7c614639a4c7b99e0836020c637abf58b89a3a47
MrGeek-zrh <mrgeek-zrh@outlook.com>
2024-03-01 14:28:33 +0800

    fix(DP): fix rrf_tb caused by RRF's code adjusting

--- testbench/verilator/DP/rrf_tb.cpp
+++ testbench/verilator/DP/rrf_tb.cpp
@@ -23,6 +23,23 @@ void dut_reset(std::shared_ptr<VRrf> dut, vluint64_t &sim_time) {
         dut->forward_rrf_we_alu1_i = 0;
         dut->forward_rrftag_alu1_i = 0;
         dut->forward_rrfdata_alu1_i = 0;
+
+        dut->forward_rrf_we_alu2_i = 0;
+        dut->forward_rrftag_alu2_i = 0;
+        dut->forward_rrfdata_alu2_i = 0;
+
+        dut->forward_rrf_we_ldst_i = 0;
+        dut->forward_rrftag_ldst_i = 0;
+        dut->forward_rrfdata_ldst_i = 0;
+
+        dut->forward_rrf_we_mul_i = 0;
+        dut->forward_rrftag_mul_i = 0;
+        dut->forward_rrfdata_mul_i = 0;
+
+        dut->forward_rrf_we_branch_i = 0;
+        dut->forward_rrftag_branch_i = 0;
+        dut->forward_rrfdata_branch_i = 0;
+
         dut->allocate_rrf_en_i = 0;
         dut->allocate_rrftag_i = 0;
         dut->completed_dst_rrftag_i = 0;
@@ -57,9 +74,11 @@ int main(int argc, char **argv, char **env) {
         vluint64_t rrftag_rand_2;
         if (dut->clk_i == 1) {
             if (posedge_cnt == 2 && sim_time == 10) {
+#ifndef WAVE
                 assert(dut->rs1_rrfvalid_o == 0);
                 assert(dut->rs2_rrfvalid_o == 0);
                 std::cout << "Rrf Test 1 Pass!" << std::endl;
+#endif  // !WAVE
 
                 rrftag_rand_1 = rand() % 64;
                 dut->allocate_rrf_en_i = 1;
@@ -74,13 +93,17 @@ int main(int argc, char **argv, char **env) {
                 dut->allocate_rrf_en_i = 1;
                 dut->allocate_rrftag_i = rrftag_rand_2;
             } else if (posedge_cnt == 4 && sim_time == 30) {
+#ifndef WAVE
                 assert(dut->rs1_rrfvalid_o == 0);
                 std::cout << "Rrf Test 2 Pass!" << std::endl;
+#endif
 
                 dut->rs2_rrftag_i = rrftag_rand_2;
             } else if (posedge_cnt == 5 && sim_time == 40) {
+#ifndef WAVE
                 assert(dut->rs2_rrfvalid_o == 0);
                 std::cout << "Rrf Test 3 Pass!" << std::endl;
+#endif
 
                 dut->forward_rrf_we_alu1_i = 1;
                 dut->forward_rrftag_alu1_i = rrftag_rand_1;
@@ -92,20 +115,26 @@ int main(int argc, char **argv, char **env) {
                 dut->forward_rrftag_alu1_i = rrftag_rand_2;
                 dut->forward_rrfdata_alu1_i = 12;
             } else if (posedge_cnt == 7 && sim_time == 60) {
+#ifndef WAVE
                 assert(dut->rs1_rrfdata_o == 11);
                 assert(dut->rs1_rrfvalid_o == 1);
                 std::cout << "Rrf Test 4 Pass!" << std::endl;
+#endif
 
                 dut->rs2_rrftag_i = rrftag_rand_2;
             } else if (posedge_cnt == 8 && sim_time == 70) {
+#ifndef WAVE
                 assert(dut->rs2_rrfdata_o == 12);
                 assert(dut->rs2_rrfvalid_o == 1);
                 std::cout << "Rrf Test 5 Pass!" << std::endl;
+#endif
 
                 dut->completed_dst_rrftag_i = rrftag_rand_2;
             } else if (posedge_cnt == 9 && sim_time == 80) {
+#ifndef WAVE
                 assert(dut->data_to_arfdata_o == 12);
                 std::cout << "Rrf Test 6 Pass!" << std::endl;
+#endif
             }
         }
 
